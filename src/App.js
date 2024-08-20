9f65e18f7ffca18523bbf8fbde23ff0d97d1d2de
LAPTOP-PK8JHPSO\ADMIN <nguyenminhtri90602003@gmail.com>
2024-08-20 18:20:54 +0700

    [React]:  Hello world, increase or decrease number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  // Khai báo biến number, biến lưu trữ là setNumber
+  const [number, setNumber] = useState(0);
+
+  // khai báo biến handleChange để sử lý việc onclick
+  // Tham số result
+  const handleChange = (result) => {
+    setNumber(number + result)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'>"Hello world</h1>
+
+      <div className='number'>
+        <button className='btn' onClick={() => handleChange(-1)}>-</button>
+        <h3>{number}</h3>
+        <button className='btn' onClick={() => handleChange(1)}>+</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
